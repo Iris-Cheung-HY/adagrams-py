7c456a3789bb1d400609dede2f720451b85d1d53
Iris Cheung <hokyinirischeung@gmail.com>
2025-09-14 00:28:15 -0700

    completed 2nd and 3rd function and both 2nd and 3rd test passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,7 +29,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+SCORE_CHART = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 4, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 1, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 4, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
 
+}
 
 def draw_letters():
     #empty list to store all the letter
@@ -51,15 +79,40 @@ def draw_letters():
         
     return letter_strings_ten
 
-
-
-
-
 def uses_available_letters(word, letter_bank):
-    pass
+    letter_bank_copy = letter_bank.copy()
+    for letter in word.upper():
+        if letter in letter_bank_copy:
+            letter_bank_copy.remove(letter)
+        else:
+            return False
+    return True
 
 def score_word(word):
-    pass
+    total_score = 0
+    for letter in word.upper():
+        total_score += SCORE_CHART[letter]
+    if len(word) >= 7 and len(word) <= 10:
+        total_score += 8
+    return total_score
+
 
 def get_highest_word_score(word_list):
-    pass
+    highest_word_score = tuple([])
+    loop_score = 0
+    highest_score = 0
+
+    for word in word_list:
+        for letter in word.upper():
+            loop_score += SCORE_CHART[letter]
+            if loop_score > highest_score:
+                highest_score = loop_score
+                highest_word_score([0]) = word
+                highest_word_score([1]) = highest_score
+    return highest_word_score
+            
+
+
+
+
+
